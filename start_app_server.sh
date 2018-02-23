5a83121b0543827066920ff7f72aa298cbb3f22c
redhoyasa <redhoyasa@redhoyasas-MacBook-Pro.local>
2018-02-23 14:03:48 +0800

    removed mongo credential

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://ganteng:ganteng@ds147518.mlab.com:47518/ganteng
+# export MONGODB_CREDENTIALS=mongodb://ganteng:ganteng@ds147518.mlab.com:47518/ganteng
 
 # finally, run the app in dev mode...
 npm run dev
